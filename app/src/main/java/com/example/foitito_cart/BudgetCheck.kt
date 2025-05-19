2fbcec431516115388fa0b6aea1f3a6f76549ebf
DimitraEmm <92228237+DimitraEmm@users.noreply.github.com>
2025-05-19 23:13:25 +0300

    0.05.0 Final Screen logo addition

--- app/src/main/java/com/example/foitito_cart/BudgetCheck.kt
+++ app/src/main/java/com/example/foitito_cart/BudgetCheck.kt
@@ -1,6 +1,10 @@
 package com.example.foitito_cart
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.view.Window
+import android.widget.Button
 import android.widget.EditText
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -8,10 +12,16 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import com.example.foitito_cart.ui.theme.Foitito_cartTheme
 
 class BudgetCheck : ComponentActivity() {
@@ -35,5 +45,21 @@ fun FinalScreen(modifier: Modifier) {
             contentDescription = null,
             contentScale = ContentScale.FillBounds
         )
+        Image(
+            painter = painterResource(R.drawable.background),
+            contentScale = ContentScale.Crop,
+            contentDescription = null
+        )
+        Image(
+            painter = painterResource(R.drawable._cart),
+            contentDescription = null,
+            modifier = Modifier.align(Alignment.Center).offset(0.dp, (-200).dp).size(280.dp)
+        )
+        Image(
+            painter = painterResource(R.drawable.logo),
+            contentDescription = null,
+            modifier = Modifier.align(Alignment.Center).offset(0.dp, (-80).dp).size(80.dp)
+        )
+
     }
 }
